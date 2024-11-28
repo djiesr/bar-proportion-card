163bb664a92a7d9d4b2c66f6fbaaa2c936b5484c
djiesr <31359825+djiesr@users.noreply.github.com>
2024-11-27 20:46:16 -0500

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/bar-proportion-card.js',
+  mode: 'production',
+  output: {
+    filename: 'bar-proportion-card.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 8081,
+    hot: true,
+  },
+};
