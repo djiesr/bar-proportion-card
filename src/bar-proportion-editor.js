690d9b25ce1bc42a8db60ac50f757a6df04b6b2a
djiesr <31359825+djiesr@users.noreply.github.com>
2024-11-27 20:40:54 -0500

    Update bar-proportion-editor.js

--- src/bar-proportion-editor.js
+++ src/bar-proportion-editor.js
@@ -60,7 +60,178 @@ class BarProportionCardEditor extends HTMLElement {
     this._buildForm();
   }
 
-  // ... (reste du code de l'éditeur)
+  _buildForm() {
+    if (this.shadowRoot) {
+      this.shadowRoot.lastChild.remove();
+    }
+
+    const helper = document.createElement('div');
+    helper.innerHTML = `
+      <style>${BarProportionCardEditor.styles}</style>
+      <div class="form-container">
+        <div class="row">
+          <ha-textfield
+            label="Titre"
+            .value="${this._title}"
+            @change="${this._valueChanged}"
+            id="title"
+          ></ha-textfield>
+        </div>
+        
+        <div class="row">
+          <ha-select
+            label="Mode d'affichage"
+            .value="${this._display_mode}"
+            @selected="${this._displayModeChanged}"
+            id="display_mode"
+          >
+            <mwc-list-item value="percentage">Pourcentage</mwc-list-item>
+            <mwc-list-item value="value">Valeur</mwc-list-item>
+          </ha-select>
+        </div>
+
+        <div class="row">
+          <ha-textfield
+            label="Décimales"
+            type="number"
+            min="0"
+            max="5"
+            .value="${this._decimals}"
+            @change="${this._valueChanged}"
+            id="decimals"
+          ></ha-textfield>
+        </div>
+
+        <div class="row">
+          <ha-textfield
+            label="Unité"
+            .value="${this._unit}"
+            @change="${this._valueChanged}"
+            id="unit"
+          ></ha-textfield>
+        </div>
+
+        <div class="row">
+          <label>Entités</label>
+          <div class="entities" id="entities">
+            ${this._entities.map((entity, index) => this._createEntityRow(entity, index)).join('')}
+          </div>
+          <mwc-button @click="${this._addEntity}">Ajouter une entité</mwc-button>
+        </div>
+      </div>
+    `;
+
+    const root = this.attachShadow({ mode: 'open' });
+    root.appendChild(helper.children[0]);
+    root.appendChild(helper.children[0]);
+    this._initializeElements();
+  }
+
+  _initializeElements() {
+    this._entities.forEach((entity, index) => {
+      const entitySelect = this.shadowRoot.querySelector(`#entity-${index}`);
+      if (entitySelect) {
+        entitySelect.addEventListener('value-changed', (ev) => {
+          this._entityChanged(index, ev.detail.value);
+        });
+      }
+    });
+  }
+
+  _createEntityRow(entity, index) {
+    return `
+      <div class="entity-row">
+        <ha-entity-picker
+          id="entity-${index}"
+          .value="${entity.entity}"
+          .label="Entité ${index + 1}"
+          .hass="${this.hass}"
+          allow-custom-entity
+        ></ha-entity-picker>
+        <ha-textfield
+          .value="${entity.name || ''}"
+          .label="Nom"
+          id="name-${index}"
+          @change="${(ev) => this._nameChanged(index, ev)}"
+        ></ha-textfield>
+        <ha-textfield
+          .value="${entity.color || ''}"
+          .label="Couleur"
+          id="color-${index}"
+          @change="${(ev) => this._colorChanged(index, ev)}"
+        ></ha-textfield>
+        <ha-icon
+          icon="mdi:delete"
+          @click="${() => this._removeEntity(index)}"
+        ></ha-icon>
+      </div>
+    `;
+  }
+
+  _addEntity() {
+    const entities = [...this._entities];
+    entities.push({
+      entity: '',
+      name: '',
+      color: ''
+    });
+    this._updateConfig({ entities });
+  }
+
+  _removeEntity(index) {
+    const entities = [...this._entities];
+    entities.splice(index, 1);
+    this._updateConfig({ entities });
+  }
+
+  _entityChanged(index, value) {
+    const entities = [...this._entities];
+    entities[index] = {
+      ...entities[index],
+      entity: value
+    };
+    this._updateConfig({ entities });
+  }
+
+  _nameChanged(index, ev) {
+    const entities = [...this._entities];
+    entities[index] = {
+      ...entities[index],
+      name: ev.target.value
+    };
+    this._updateConfig({ entities });
+  }
+
+  _colorChanged(index, ev) {
+    const entities = [...this._entities];
+    entities[index] = {
+      ...entities[index],
+      color: ev.target.value
+    };
+    this._updateConfig({ entities });
+  }
+
+  _displayModeChanged(ev) {
+    this._updateConfig({ display_mode: ev.target.value });
+  }
+
+  _valueChanged(ev) {
+    const target = ev.target;
+    this._updateConfig({
+      [target.id]: target.value
+    });
+  }
+
+  _updateConfig(updates) {
+    const newConfig = {
+      ...this._config,
+      ...updates
+    };
+    const event = new CustomEvent('config-changed', {
+      detail: { config: newConfig }
+    });
+    this.dispatchEvent(event);
+  }
 }
 
 customElements.define('bar-proportion-card-editor', BarProportionCardEditor);
